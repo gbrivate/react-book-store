89c2637f3eb0cd0dbc0a75eb7bfbb6c29b6850c1
Gabriel Santos <gabrielinflex@gmail.com>
2020-04-22 21:54:28 -0300

    Updates

---
+++ precache-manifest.fc64dc570c733659b11e89b3001ca2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db0ddf9d3f3d0a1f1bca033edcb86aa",
+    "url": "/react-book-store/index.html"
+  },
+  {
+    "revision": "928a5fffc3a1d35f0039",
+    "url": "/react-book-store/static/css/main.0eb9c55d.chunk.css"
+  },
+  {
+    "revision": "ed8c24cb80ec8a688689",
+    "url": "/react-book-store/static/js/2.8a0efcf9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-book-store/static/js/2.8a0efcf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "928a5fffc3a1d35f0039",
+    "url": "/react-book-store/static/js/main.884f05bd.chunk.js"
+  },
+  {
+    "revision": "28e0955a386ce5000efd",
+    "url": "/react-book-store/static/js/runtime-main.370c5588.js"
+  }
+]);
